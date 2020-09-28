0404c2ce770a726a7c63026890a67ce9471db2ad
09131968daniel <dconstantinescu@hotmail.com>
2020-09-28 19:07:39 -0400

    equipment.service moved too

--- src/app/services/exercise-category.service.ts
+++ src/app/services/exercise-category.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IWgerde } from '../Interfaces/IWgerde';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExerciseCategoryService {
-
-  constructor() { }
+  productURL = 'https://wger.de/api/v2/exercisecategory?language=2&license_author=wger.de&limit=66';
+  constructor(private http: HttpClient) { }
+  getExerciseCategory(): Observable<IWgerde> {
+    return this.http.get<IWgerde>(this.productURL);
+  }
 }
