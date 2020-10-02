1470cea531f70d628d88bc35fad3c39dfc03d12d
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2020-10-02 13:48:40 -0700

    Added Name Rendering of Muscles, Equipment, Category

--- src/app/exercise/exercise.component.ts
+++ src/app/exercise/exercise.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EquipmentService } from '@app/services/equipment.service';
 import { ExerciseCategoryService } from '@app/services/exercise-category.service';
 import { ExerciseService } from '@app/services/exercise.service';
+import { MuscleService } from '@app/services/muscle.service';
 import { IExercise } from '../Interfaces/IExercise';
 
 @Component({
@@ -18,18 +20,56 @@ export class ExerciseComponent implements OnInit {
   constructor(
     private exercise: ExerciseService,
     private category: ExerciseCategoryService,
+    private muscle: MuscleService,
+    private equipment: EquipmentService,
   ) { }
   ngOnInit(): void {
     this.exercise.getExercises().subscribe({
       next: (data) => {
         this.exercises = data as unknown as IExercise[];
-        this.category.getExerciseCategory().subscribe({
-          next: (data) => {
-
-          }
-        })
-        console.log(this.exercise);
+        this.addCategoryName();
+        this.addMuscleName();
+        this.addEquipmentName();
       }
     });
+  }
+
+  addCategoryName() {
+    this.category.getExerciseCategory().subscribe({
+      next: (data) => {
+        for (let exercise of this.exercises) {
+          let foundCategory = data.find((cat) => (cat.id == exercise['category']));
+          exercise.categoryName = foundCategory.name;
+        }
+      }
+    })
+  }
+
+  addMuscleName() {
+    this.muscle.getMuscles().subscribe({
+      next: (data) => {
+        for (let exercise of this.exercises) {
+          let foundMuscles = [];
+          for (let muscleInExcercise of exercise.muscles) {
+            foundMuscles.push(data.find((muscle) => (muscle.id == muscleInExcercise)));
+          }
+          exercise.muscles = foundMuscles;
+        }
+      }
+    })
+  }
+
+  addEquipmentName() {
+    this.equipment.getEquipment().subscribe({
+      next: (data) => {
+        for (let exercise of this.exercises) {
+          let foundEquipment = [];
+          for (let equipmentInExercise of exercise.equipment) {
+            foundEquipment.push(data.find((equipment) => (equipment.id == equipmentInExercise)));
+          }
+          exercise.equipment = foundEquipment;
+        }
+      }
+    })
   }
 }
