afe07391b351a3881e57dedb7bd2db107c10f9a9
Philip Horton <philip.horton@att.net>
2020-10-02 14:57:36 -0600

    quick multiple sets fix

--- src/app/sets/sets.component.ts
+++ src/app/sets/sets.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SetsService } from '../services/sets.service';
-import { ISets } from '../Interfaces/ISets';
+import { ISets, ISetExercises } from '../Interfaces/ISets';
 import { ExerciseService } from '../services/exercise.service';
 import { IExercise } from '../Interfaces/IExercise';
+import { AccountService } from '../_services';
 
 @Component({
   selector: 'app-sets',
@@ -10,14 +11,16 @@ import { IExercise } from '../Interfaces/IExercise';
   styleUrls: ['./sets.component.css']
 })
 export class SetsComponent implements OnInit {
-  userId: number = 1;
   sets: any[] = [];
+  userSet: ISets[] = [];
   allExercises: any[] = [];
   setExcercises: any[] = [];
+  setAmount: number[] = [];
 
   constructor(
     private set: SetsService,
     private exercise: ExerciseService,
+    private account: AccountService,
   ) { }
 
   ngOnInit(): void {
@@ -29,14 +32,22 @@ export class SetsComponent implements OnInit {
             this.allExercises = exerciseData as unknown as IExercise[];
             
             //Implement user login here; going to grab the 1st user for now
-            console.log(this.allExercises);
-            for (let set of this.sets[0].exercises) {
-              console.log(set.id);
-              let value = this.allExercises.find(x => x.id == set.id);
-              console.log(value);
-              this.setExcercises.push(value);
-            }
+            // console.log(this.account.userValue.id);
+            // console.log(this.account.userValue.username);
+            this.userSet = this.sets.filter(x => x.user == this.account.userValue.id);
+            console.log(this.userSet);
+            // for (let set of this.userSet) {
+            //   for (let exerc of set.exercises) {
+            //     console.log(exerc.id);
+            //     let value = this.allExercises.find(x => x.id == exerc.id);
+            //     console.log(value);
+            //     this.setExcercises.push(value);
+            //     this.setAmount.push(exerc.amount);
+            //   }
+            // }
+            
             console.log(this.setExcercises);
+            console.log(this.setAmount);
           }
         });
       }
